1817c6de975981ed0128797621977c0857ad1d91
Ray <rjmaestas1991@gmail.com>
2022-07-21 12:17:54 -0600

    commit tree 9c78effcba1a9a9894672ecaa055609f51e0185d

--- test/index-test.js
+++ test/index-test.js
@@ -1,13 +1,13 @@
 const { name, height, message } = require("../index.js");
 
 describe("what-is-a-test", () => {
-  describe("Name", () => {
+  describe("Susan", () => {
     it('returns "Susan"', () => {
       expect(name).toEqual("Susan");
     });
   });
 
-  describe("Height", () => {
+  describe("39", () => {
     it("is less than 40", () => {
       expect(height).toBeLessThan(40);
     });
